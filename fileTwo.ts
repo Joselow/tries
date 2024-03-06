b6ced4e828bdf6a0ef4dcc6a8769b6552b21c20b
Joselow <Joselow>
2024-03-06 15:47:43 -0500

    owo

--- fileTwo.ts
+++ fileTwo.ts
@@ -1,1 +1,8 @@
 console.log('APRENDE ST WN')
+console.log('CALLA MANCO APRENDE JS MEJOR');
+
+console.log('MEJOREN EN LO BASICO');
+
+console.log('DEPUES NO SABEN NI MR**');
+console.log('MANCOS TODOS');
+
